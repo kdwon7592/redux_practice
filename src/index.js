3d9f51cff431743d75505af16f40e495591c111d
dowon <dowon@dowonui-MacBookAir.local>
2022-06-08 23:19:36 +0900

    Redux Directory 분리

--- src/index.js
+++ src/index.js
@@ -1,66 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-
-const initialState = {
-  task: '',
-  tasks: []
-};
-
-const taskReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INPUT_TASK':
-      return {
-        ...state,
-        task: action.payload.task
-      };
-
-    case 'ADD_TASK':
-      return {
-        ...state,
-        tasks: state.tasks.concat([action.payload.task])
-      };
-
-    default:
-      return state;
-  }
-};
+import TodoApp from './components/TodoApp';
+import { taskReducer } from './reducers/task';
 
 const store = createStore(taskReducer);
-
-const addTask = (task) => ({
-  type: 'ADD_TASK',
-  payload: {
-    task
-  }
-});
-
-const inputTask = (task) => ({
-  type: 'INPUT_TASK',
-  payload: {
-    task
-  }
-});
-
-function TodoApp({ store }) {
-  const { task, tasks } = store.getState();
-
-  return (
-    <div>
-      <input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
-      <input type="button" value="add" onClick={() => store.dispatch(addTask(task))} />
-      <ul>
-        {
-          tasks.map((item, i) => {
-            return (
-              <li key={i}>{item}</li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
-}
 
 function renderApp(store) {
   render(
