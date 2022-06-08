ba33fb981e1f7626fa9e7fc4b77b199c1e3bfdce
dowon <dowon@dowonui-MacBookAir.local>
2022-06-08 23:10:21 +0900

    Redux TEST

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+import React from 'react';
 
-function App() {
+
+const initialState = {
+  task: '',
+  tasks: [],
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INPUT_TASK':
+      return {
+        ...state,
+        task: action.payload.task
+      };
+
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.task])
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(taskReducer);
+
+const addTask = (task) => ({
+  type: 'ADD_TASK',
+  payload: {
+    task
+  }
+});
+
+const inputTask = (task) => ({
+  type: 'INPUT_TASK',
+  payload: {
+    task
+  }
+});
+
+function TodoApp({ store }) {
+  const [task, tasks] = store.getState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={(e) => store.dispatch(inputTask(e.target.value))} />
+      <input type="button" value="add" onClick={() => store.dispatch(addTask(task))} />
+      <ul>
+        {
+          tasks.map((item, i) => {
+            return (
+              <li key={i}>{item}</li>
+            );
+          })
+        }
+      </ul>
     </div>
   );
 }
+
+function App(store) {
+  return (
+    <TodoApp store={store}></TodoApp>
+  );
+}
+
+store.subscribe(() => App(store));
 
 export default App;
